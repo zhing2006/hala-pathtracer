a2cbf1aae9bd2ef03cae1defdc059b19d91c723e
zhing2006 <382601573@qq.com>
2024-09-11 19:56:08 +0800

    Update gfx, imgui and renderer.

--- src/bin/hala-pathtracer.rs
+++ src/bin/hala-pathtracer.rs
@@ -213,7 +213,7 @@ impl HalaApplicationContextTrait for PathTracerApplicationContext {
     renderer.commit()?;
 
     self.imgui = Some(HalaImGui::new(
-      std::rc::Rc::clone(&(*renderer.resources().context)),
+      std::rc::Rc::clone(&renderer.resources().context),
       false,
     )?);
 
